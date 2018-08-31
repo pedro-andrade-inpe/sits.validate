fcbad71ae010b7c54da4cb0b617d8fbe34a17101
pedro-andrade-inpe <pedro.andrade@inpe.br>
2018-08-31 10:27:43 -0300

    Creating a terraclass tif file that can be compared pixel by pixel with sits.

---
+++ demo/buildComparableTerraClass.R
@@ -0,0 +1,37 @@
+
+require(sits.validate)
+
+sitstif <- baseDir("classificacoes-final/result-cerrado_2012_8_2013_8.tif")
+splitDir <- normalizePath(baseDir("cerrado/terraClass/split"))
+
+splitRaster(sitstif, splitDir, 8) # split into 64 tif files
+
+files <- list.files(splitDir)
+
+tctif <- baseDir("cerrado/terraClass/TCCerrado_2013_geo.tif")
+tccerrado <- raster::raster(tctif)
+
+resultDir <- normalizePath(baseDir("cerrado/terraClass/result"))
+
+for(file in files){
+  cat(paste0("Processing ", file, "\n"))
+  myraster <- raster::raster(paste0(splitDir, "/", file)) %>% raster::raster()
+
+  result <- raster::projectRaster(tccerrado, myraster, method = "ngb",
+    file = paste0(resultDir, "/", file), overwrite = TRUE)
+}
+
+# join the results
+rasters <- lapply(files, function(file){
+  cat(paste0("Reading file '", file, "'\n"))
+  paste0(resultDir, "/", file) %>% raster::raster()
+})
+
+comparableDir <- normalizePath(baseDir("cerrado/terraClass/comparable"))
+
+outputFile <- paste0(comparableDir, "/terraclass.tif") %>% normalizePath(mustWork = FALSE)
+
+cat(paste0("Creating '", outputFile, "'\n"))
+rasters$filename <- outputFile
+
+do.call(raster::merge, rasters)
