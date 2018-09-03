b3d2d7c45384e76e0b6cad2c84f5a08bcc324bbe
pedro-andrade-inpe <pedro.andrade@inpe.br>
2018-09-03 13:55:48 -0300

    Small updates.

--- demo/buildComparableProdesMask.R
+++ demo/buildComparableProdesMask.R
@@ -3,23 +3,24 @@ require(sits.validate)
 
 sitstif <- baseDir("classificacoes-final/result-cerrado_2012_8_2013_8.tif")
 
-# data must be reprojected prevously as it takes too much time in R
 #mask1 <- sf::read_sf(dsn = baseDir("cerrado/prodesMask"), layer = "MASC_CERR_2000_2015_lote_final_cor_pol")
 #mask2 <- sf::read_sf(dsn = baseDir("cerrado/prodesMask"), layer = "MASC_CERR_2000_2015_lote_final_cor_pol_split1")
 #mask3 <- sf::read_sf(dsn = baseDir("cerrado/prodesMask"), layer = "MASC_CERR_2000_2015_lote_final_cor_pol_split2")
 
+# data was reprojected
 mask1 <- sf::read_sf(dsn = baseDir("cerrado/prodesMask/reproj"), layer = "mask1")
 mask2 <- sf::read_sf(dsn = baseDir("cerrado/prodesMask/reproj"), layer = "mask2")
 mask3 <- sf::read_sf(dsn = baseDir("cerrado/prodesMask/reproj"), layer = "mask3")
 
+# removing water polygons
+mask1 <- mask1[mask1$class_name %in% paste0("D_", 2000:2015),]
+mask2 <- mask2[mask2$class_name %in% paste0("D_", 2000:2015),]
+mask3 <- mask3[mask3$class_name %in% paste0("D_", 2000:2015),]
+
 mask <- rbind(mask1, mask2, mask3)
 
-rm(mask1)
-rm(mask2)
-rm(mask3)
+rm(mask1, mask2, mask3)
 gc()
-
-## TODO: Remove WATER mask!!
 
 sitsraster <- raster::raster(sitstif) %>% raster::raster()
 result <- fasterize::fasterize(mask, sitsraster)
--- demo/validateProdesMaskComparable.R
+++ demo/validateProdesMaskComparable.R
@@ -2,25 +2,25 @@
 require(sits.validate)
 
 prodesmask2013 <- raster::raster(baseDir("cerrado/prodesMask/comparable/prodesmask.tif"))
-sits2013 <- raster::raster(baseDir("classificacoes-final/result-cerrado_2013_8_2014_8.tif"))
+sits2013 <- raster::raster(baseDir("classificacoes-final/result-cerrado_2014_8_2015_8.tif"))
 
 bs <- raster::blockSize(sits2013)
 total <- data.frame()
 
 for(i in 1:bs$n){
   cat(paste0("Processing block ", i, "/", bs$n, "\n"))
-  row = bs$row[i]
-  nrows = bs$nrows[i]
+  row <- bs$row[i]
+  nrows <- bs$nrows[i]
 
-  blockpr = raster::getValues(prodesmask2013, row = row, nrows = nrows)
+  blockpr <- raster::getValues(prodesmask2013, row = row, nrows = nrows)
   blockpr[is.na(blockpr)] <- 0
 
-  blocksi = raster::getValues(sits2013, row = row, nrows = nrows)
+  blocksi <- raster::getValues(sits2013, row = row, nrows = nrows)
 
   for(prvalue in unique(blockpr)){
     sivalues = blocksi[which(blockpr == prvalue)] %>% table() %>% as.data.frame()
     if(dim(sivalues)[1] > 0){
-      sivalues[,"Var2"] = prvalue
+      sivalues[, "Var2"] = prvalue
       total <- rbind(total, sivalues)
     }
   }
@@ -28,14 +28,14 @@ for(i in 1:bs$n){
 
 names(total) <- c("sits", "count", "prodes")
 tib <- tibble::as_tibble(total) %>% dplyr::group_by(sits, prodes) %>% dplyr::summarize_all(sum)
-result <- xtabs(count~sits+prodes,tib) %>% as.matrix()
+result <- xtabs(count ~ sits +prodes, tib) %>% as.matrix()
 
 result
-colnames(result) <- getSitsValidateEnv()$classificacao_tc
+colnames(result) <- getSitsValidateEnv()$classes_mask
 rownames(result) <- getSitsValidateEnv()$classes_sits[-14] # removing sugarcane class
 
-totalValidationCerradoMask(result) # 0.744
+totalValidationCerradoMask(result) # 0.729
 
 res <- summarizeAsPercentage(result)
 res <- round(res, 2)
-write.table(res, "result-tc.csv", sep=",")
+write.table(res, "result-pm.csv", sep=",")
