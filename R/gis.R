594f14017507f92b5e15caff258089090b3a193e
pedro-andrade-inpe <pedro.andrade@inpe.br>
2019-02-15 14:31:24 -0200

    Functions to resample a raster and count pixels.

--- R/gis.R
+++ R/gis.R
@@ -38,3 +38,98 @@ getPalette <- function(filename = baseDir("style_cerrado_13classes.qml")){
 
   return(palette)
 }
+
+#' @title Resample raster data
+#' @description Resample a raster file using the resolution and extent of a
+#' reference data.
+#' @param inputFile File name to be resampled.
+#' @param outputFile File name to be created.
+#' @param reference The reference data whose resolution and extent will be
+#' used for the otput file.
+#' @export
+reSample <- function(inputFile, outputFile, reference){
+  referenceData <- baseDir(reference)
+  path <- dirname((baseDir(inputFile)))
+
+  splitDir <- paste0(path, "/split")
+  dir.create(splitDir)
+
+  resultDir <- baseDir(paste0(dirname(outputFile), "/result"))
+  dir.create(resultDir)
+
+  splitRaster(referenceData, paste0(dirname(inputFile), "/split"), 8) # split into 64 tif files
+
+  files <- list.files(splitDir)
+
+  lapigtif <- baseDir(inputFile)
+  lapig <- raster::raster(lapigtif)
+
+  for(file in files){
+    cat(paste0("Processing ", file, "\n"))
+    myraster <- raster::raster(paste0(splitDir, "/", file)) %>% raster::raster()
+
+    result <- raster::projectRaster(lapig, myraster, method = "ngb",
+                                    file = paste0(resultDir, "/", file), overwrite = TRUE)
+  }
+
+  # join the results
+  rasters <- lapply(files, function(file){
+    cat(paste0("Reading file '", file, "'\n"))
+    paste0(resultDir, "/", file) %>% raster::raster()
+  })
+
+  outputFile <- baseDir(outputFile) %>% normalizePath(mustWork = FALSE)
+
+  if(file.exists(outputFile)) file.remove(outputFile)
+
+  cat(paste0("Creating '", outputFile, "'\n"))
+  rasters$filename <- outputFile
+
+  do.call(raster::merge, rasters)
+
+  unlink(splitDir, recursive = TRUE)
+  unlink(resultDir, recursive = TRUE)
+}
+
+#' @title Compare two rasters
+#' @description Generate a confusion matrix to compare two rasters.
+#' @param data String with the input data file.
+#' @param reference String with the reference data file.
+#' @export
+compareRasters <- function(data, reference){
+  referenceRaster <- raster::raster(reference)
+  dataRaster <- raster::raster(data)
+
+  total <- data.frame()
+
+  forEachBlockPair(referenceRaster, dataRaster, function(blockpr, blocksi){
+    blockpr[is.na(blockpr)] <- 0
+
+    for(prvalue in unique(blockpr)){
+      sivalues = blocksi[which(blockpr == prvalue)] %>% table() %>% as.data.frame()
+      if(dim(sivalues)[1] > 0){
+        sivalues[, "Var2"] = prvalue
+        total <<- rbind(total, sivalues)
+      }
+    }
+  })
+
+  names(total) <- c("data", "count", "reference")
+  tib <- tibble::as_tibble(total) %>% dplyr::group_by(data, reference) %>% dplyr::summarize_all(sum)
+
+  xtabs(count ~ data + reference, tib) %>%
+    as.matrix() %>%
+    summarizeAsPercentage() %>%
+    round(2)
+}
+
+#' @title Count pixels of each class
+#' @description Count the number of pixels of each class in a raster file.
+#' @param filename String with the file name.
+#' @export
+countPixels <- function(filename){
+  filename %>%
+    raster::raster() %>%
+    getValues %>%
+    table
+}
