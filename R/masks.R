5f24106046a3d95fd55877052c6505eab10578b2
pedro-andrade-inpe <pedro.andrade@inpe.br>
2019-09-10 21:54:57 -0300

    Functions to post-process Cerrado.

--- R/masks.R
+++ R/masks.R
@@ -1,16 +1,30 @@
 
-#' @title Applies water mask
-#' @description Updates the pixels that overlap the water pixels
-#' into water. This function only works for Brazil.
-#' @param myraster A raster.
-#' @param legend A tibble with the legend, as described by readLegend().
+#' @title Apply cerrado mask
+#' @description Apply cerrado biome mask in order to remove all pixels that do not
+#' belong to the biome
+#' @param myraster A raster data.
 #' @export
-waterMask <- function(myraster, legend){
-  cat("Applying water mask\n")
-  water <- raster::raster(baseDir("masks/waterMask.tif"))
+cerradoMask <- function(myraster){
+  cat("Applying cerrado mask\n")
 
-  water.sub <- raster::crop(water, raster::extent(myraster))
-  waterClass <- csv$Label %>% str_detect("Water") %>% which()
+  cerrado <- raster::raster(baseDir("masks/cerrado/cerradoMask.tif"))
+  cerrado.sub <- raster::crop(cerrado, raster::extent(myraster))
+
+  raster::mask(myraster, cerrado.sub, progress = "text")
+}
+
+#' @title Apply cerrado water mask
+#' @description Apply cerrado water mask from Perkel et al. (doi:10.1038/nature20584) over a raster, given a legend and an year.
+#' @param myraster A raster.
+#' @param legend A tibble legend.
+#' @export
+cerradoWaterMask <- function(myraster, legend){
+  cat("Applying water mask\n")
+  water.sub <- baseDir("masks/cerrado/cerradoWaterMask.tif") %>%
+    raster::raster() %>%
+    raster::crop(raster::extent(myraster))
+
+  waterClass <- legend$Label %>% stringr::str_detect("Water") %>% which()
 
   if(length(waterClass) > 1){
     stop("waterMask() cannot be used as there are more than one 'Water' classes in the legend")
@@ -22,16 +36,21 @@ waterMask <- function(myraster, legend){
   raster::mask(myraster, water.sub, maskvalue = 1, updatevalue = waterClass, progress = "text")
 }
 
-urbanMask <- function(myraster, year, legend){
+#' @title Apply cerrado urban mask
+#' @description Apply cerrado urban mask from ?? over a raster, given a legend and an year.
+#' @param myraster A raster.
+#' @param legend A tibble legend.
+#' @param year An year. Sugarcane data are available for 2000 to 2016.
+#' @export
+cerradoUrbanMask <- function(myraster, legend, year){
   cat("Applying urban mask\n")
 
-  urban <- paste0("masks/urban_areas_", year, ".tif") %>%
+  urban.sub <- paste0("masks/cerrado/urban_areas_", year, ".tif") %>%
     baseDir %>%
-    raster::raster()
+    raster::raster() %>%
+    raster::crop(raster::extent(myraster))
 
-  urban.sub <- raster::crop(urban, raster::extent(myraster))
-
-  urbanClass <- legend$Label %>% str_detect("Urban") %>% which()
+  urbanClass <- legend$Label %>% stringr::str_detect("Urban") %>% which()
 
   if(length(urbanClass) > 1){
     stop("urbanMask() cannot be used as there are more than one 'Urban' classes in the legend")
@@ -43,23 +62,26 @@ urbanMask <- function(myraster, year, legend){
   raster::mask(myraster, urban.sub, maskvalue = 1, updatevalue = urbanClass, progress = "text")
 }
 
-sugarcaneMask <- function(myraster, legend, year){
+#' @title Apply cerrado sugarcane mask
+#' @description Apply cerrado sugarcane mask from Canasat over a raster, given a legend and an year.
+#' @param myraster A raster.
+#' @param legend A tibble legend.
+#' @param year An year. Sugarcane data are available for 2000 to 2016.
+#' @export
+cerradoSugarcaneMask <- function(myraster, legend, year){
   cat("Applying sugarcane mask\n")
 
-  sugar <- paste0("masks/Canasat_", year, ".tif") %>%
+  sugar <- paste0("masks/cerrado/Canasat_", year, ".tif") %>%
     baseDir %>%
-    raster::raster()
+    raster::raster() %>%
+    raster::crop(raster::extent(myraster))
 
-  if(raster::extent(sugar) != raster::extent(myraster)){
-    sugar <- raster::crop(sugar, raster::extent(myraster))
-  }
+  sugarcaneClass <- legend$Label %>% stringr::str_detect("Sugarcane") %>% which()
 
-  sugarcaneClass <- csv$Label %>% str_detect("Sugarcane") %>% which()
-
-  if(length(waterClass) > 1){
+  if(length(sugarcaneClass) > 1){
     stop("sugarcaneMask() cannot be used as there are more than one 'Sugarcane' classes in the legend")
   }
-  else if(length(waterClass) == 0){
+  else if(length(sugarcaneClass) == 0){
     stop("Could not find any 'Sugarcane' class in the legend")
   }
 
